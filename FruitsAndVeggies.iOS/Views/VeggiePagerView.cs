ca216d5dae1cb6fced7bc37ed40afc043e7b0bdc
Greg Mercer (GBear) <gmercer@stanford.edu>
2017-07-03 13:57:52 -0700

    Adding swipe paging for Veggie detail pages on iOS

---
+++ FruitsAndVeggies.iOS/Views/VeggiePagerView.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+using FruitsAndVeggies.Core.ViewModels;
+using FruitsAndVeggies.iOS.Cells;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using MvvmCross.iOS.Views;
+
+using UIKit;
+
+using MvvmCross.Platform.WeakSubscription;
+using System.Collections.Generic;
+
+namespace FruitsAndVeggies.iOS.Views
+{
+    public partial class VeggiePagerView : MvxViewController<VeggiePagerViewModel>
+    {
+        private VeggiePagerViewController _pageViewController;
+
+        public VeggiePagerView() : base("VeggiePagerView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            List<string> veggieIds = ViewModel.VeggieIds.Split(',').ToList();
+
+            List<IVeggiePages> pages = new List<IVeggiePages>();
+
+            int index = 0;
+            foreach (var id in veggieIds)
+            {
+                var viewController = Activator.CreateInstance(typeof(VeggieView)) as VeggieView;
+
+                var veggieViewModel = new VeggieViewModel();
+                veggieViewModel.Name = id;
+                veggieViewModel.Init(veggieViewModel);
+
+                viewController.Title = $"Veggie {id}";
+                viewController.ViewModel = veggieViewModel;
+
+                pages.Add(viewController);
+                index++;
+            }
+
+            View = new UIView();
+
+            _pageViewController = new VeggiePagerViewController(
+                new VeggiePagerViewDataSource(pages),
+                GetCurrentPosition()
+            );
+
+            View.Add(_pageViewController.View);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        private int GetCurrentPosition()
+        {
+            int position = 0;
+
+            List<string> veggieIds = ViewModel.VeggieIds.Split(',').ToList();
+            foreach (var id in veggieIds)
+            {
+                if (id == ViewModel.VeggieId)
+                {
+                    return position;
+                }
+                position++;
+            }
+
+            return position;
+        }
+    }
+}
---
+++ FruitsAndVeggies.iOS/Views/VeggieView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FruitsAndVeggies.Core.ViewModels;
+using FruitsAndVeggies.iOS.Extensions;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+namespace FruitsAndVeggies.iOS.Views
+{
+    public partial class VeggieView : MvxViewController, IVeggiePages
+    {
+        public int PageIndex { get; set; }
+
+        public VeggieView() : base("VeggieView", null)
+        {
+        }
+
+        public VeggieView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<VeggieView, VeggieViewModel>();
+            set.Bind(Name).To(vm => vm.Name);
+            set.Apply();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            // Show the navigation bar.
+            //this.ShowNavigationBar();
+            base.ViewWillAppear(animated);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
